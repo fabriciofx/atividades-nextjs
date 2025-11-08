ca5263c8735a2356e8802ae42d6fc41a8c30e908
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-11-08 17:41:55 -0300

    feat(contador): adiciona a calculadora de contagem de cliques

---
+++ app/contador/page.tsx
@@ -0,0 +1,36 @@
+'use client';
+import { useState } from 'react';
+
+const Contador: React.FC = () => {
+  const [contagem, setContagem] = useState<number>(0);
+
+  const incrementa = () => {
+    setContagem(contagem + 1);
+  };
+
+  const decrementa = () => {
+    setContagem(contagem - 1);
+  };
+
+  return (
+    <div style={{ textAlign: 'center', margin: '20px' }}>
+      <p>Contagem atual: {contagem}</p>
+      <button
+        type="button"
+        onClick={incrementa}
+        style={{ padding: 30, fontSize: 20 }}
+      >
+        +
+      </button>
+      <button
+        type="button"
+        onClick={decrementa}
+        style={{ padding: 30, fontSize: 20 }}
+      >
+        -
+      </button>
+    </div>
+  );
+};
+
+export default Contador;
