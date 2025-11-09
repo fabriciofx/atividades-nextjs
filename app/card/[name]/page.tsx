4b527edf5b5d88ecc800ca5414931027be3e4129
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-11-09 18:52:13 -0300

    feat(card): adiciona o componente profile card

---
+++ app/card/[name]/page.tsx
@@ -0,0 +1,47 @@
+'use client';
+
+import type { ParamValue } from 'next/dist/server/request/params';
+import { useParams } from 'next/navigation';
+import ProfileCard from '@/components/profile-card/ProfileCard';
+
+type Person = {
+  name: string;
+  bio: string;
+  imageUrl: string;
+};
+
+export default function CardPage() {
+  const params = useParams();
+  const people = new Map<ParamValue, Person>([
+    [
+      'ana',
+      {
+        name: 'Ana Silva',
+        bio: 'Desenvolvedora Frontend apaixonada por Next.JS',
+        imageUrl: `http://localhost:3000/${params.name}.png`
+      }
+    ],
+    [
+      'luis',
+      {
+        name: 'Luis Cordeiro',
+        bio: 'Desenvolvedor Frontend apaixonada por React',
+        imageUrl: `http://localhost:3000/${params.name}.png`
+      }
+    ]
+  ]);
+  return (
+    <div
+      className="
+        min-h-screen flex items-center justify-center bg-gray-100
+        dark:bg-gray-900 p-4
+      "
+    >
+      <ProfileCard
+        name={people.get(params.name)?.name || ''}
+        bio={people.get(params.name)?.bio || ''}
+        imageUrl={people.get(params.name)?.imageUrl || ''}
+      />
+    </div>
+  );
+}
---
+++ components/profile-card/ProfileCard.tsx
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+
+type ProfileCardProps = {
+  name: string;
+  bio: string;
+  imageUrl: string;
+};
+
+export default function ProfileCard({ name, bio, imageUrl }: ProfileCardProps) {
+  return (
+    <div
+      className="
+        flex flex-col items-center bg-white dark:bg-gray-800 shadow-lg
+        rounded-2xl p-6 w-full max-w-sm transition-transform hover:scale-105
+        hover:shadow-2xl
+      "
+    >
+      <div className="w-32 h-32 relative mb-4">
+        <Image
+          src={imageUrl}
+          alt={`${name} profile`}
+          unoptimized
+          width={128}
+          height={128}
+          className="
+            rounded-full object-cover border-4 border-indigo-500 shadow-md
+          "
+        />
+      </div>
+      <h2 className="text-xl font-semibold text-gray-900 dark:text-white">
+        {name}
+      </h2>
+      <p className="text-gray-600 dark:text-gray-300 text-center mt-2">{bio}</p>
+    </div>
+  );
+}
