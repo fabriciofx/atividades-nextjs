16d8b291c49aeedf4510f5c376511743e19b506a
Fabrício Barros Cabral <fabriciofx@gmail.com>
2025-12-12 11:57:51 -0300

    feat: modifica a simulação de erro para que seja ativada com um botão

--- app/erro/page.tsx
+++ app/erro/page.tsx
@@ -1,9 +1,20 @@
 'use client';
 
 export default function TestErrorPage() {
-  const defeito = Math.floor(Math.random() * 2);
-  if (defeito) {
-    throw new Error('Simulação de erro proposital!');
+  function defeito(_form: FormData) {
+    throw new Error('Mensagem de erro: simulação de erro proposital!');
   }
-  return <h1>Load da página ok!</h1>;
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <form action={defeito}>
+        <button
+          type="submit"
+          className="px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white
+            font-medium rounded-xl shadow-md transition-all duration-200"
+        >
+          Simular Erro
+        </button>
+      </form>
+    </div>
+  );
 }
